3d79dbb8bf2806f13c972dd9eebfd5bce93df1fe
mccannsa <smccann144@gmail.com>
2021-11-25 12:59:14 -0500

    changed currently playing label

--- src/components/Playing.vue
+++ src/components/Playing.vue
@@ -2,7 +2,7 @@
   <div class="flex-container">
     <p id="user">Hello, <span id="displayName" @click="overview()">{{ this.displayName }}</span>.</p>
     <p v-if="this.track">
-      Currently playing:
+      You're currently listening to:
       <span id="playing" @click="link(track.name, track.artists[0].name)">{{ this.desc }}</span>
     </p>
     <p v-else>Not playing</p>
